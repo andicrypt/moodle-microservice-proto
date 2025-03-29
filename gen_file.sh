944f9d39fece32816b15a519cb974f9280b7fc1e
andicrypt <keepgoing0901@gmail.com>
2025-03-29 17:15:16 +0700

    Initial commit

---
+++ gen_file.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+SERVICE_NAME="payment"
+
+mkdir ${SERVICE_NAME}
+touch ${SERVICE_NAME}/${SERVICE_NAME}.proto
+
+echo -e "syntax = \"proto3\";\n\noption go_package = \"github.com/andicrypt/moodle-microservice-proto/${SERVICE_NAME}\";" > ${SERVICE_NAME}/${SERVICE_NAME}.proto
