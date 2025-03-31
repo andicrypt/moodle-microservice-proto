3a91df93eb16bdc070bc5b70d63b91dcba4f88d0
andicrypt <andicrypt@users.noreply.github.com>
2025-03-31 08:13:06 +0000

    Proto update - v0.1.21

--- golang/auth/auth.pb.go
+++ golang/auth/auth.pb.go
@@ -325,6 +325,102 @@ func (x *LogoutResponse) GetSuccess() bool {
 	return false
 }
 
+type RefreshAccessTokenRequest struct {
+	state         protoimpl.MessageState `protogen:"open.v1"`
+	Email         string                 `protobuf:"bytes,1,opt,name=email,proto3" json:"email,omitempty"`
+	RefreshKey    string                 `protobuf:"bytes,2,opt,name=refresh_key,json=refreshKey,proto3" json:"refresh_key,omitempty"`
+	unknownFields protoimpl.UnknownFields
+	sizeCache     protoimpl.SizeCache
+}
+
+func (x *RefreshAccessTokenRequest) Reset() {
+	*x = RefreshAccessTokenRequest{}
+	mi := &file_auth_auth_proto_msgTypes[6]
+	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+	ms.StoreMessageInfo(mi)
+}
+
+func (x *RefreshAccessTokenRequest) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*RefreshAccessTokenRequest) ProtoMessage() {}
+
+func (x *RefreshAccessTokenRequest) ProtoReflect() protoreflect.Message {
+	mi := &file_auth_auth_proto_msgTypes[6]
+	if x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use RefreshAccessTokenRequest.ProtoReflect.Descriptor instead.
+func (*RefreshAccessTokenRequest) Descriptor() ([]byte, []int) {
+	return file_auth_auth_proto_rawDescGZIP(), []int{6}
+}
+
+func (x *RefreshAccessTokenRequest) GetEmail() string {
+	if x != nil {
+		return x.Email
+	}
+	return ""
+}
+
+func (x *RefreshAccessTokenRequest) GetRefreshKey() string {
+	if x != nil {
+		return x.RefreshKey
+	}
+	return ""
+}
+
+type RefreshAccessTokenResponse struct {
+	state         protoimpl.MessageState `protogen:"open.v1"`
+	AccessKey     string                 `protobuf:"bytes,1,opt,name=access_key,json=accessKey,proto3" json:"access_key,omitempty"`
+	unknownFields protoimpl.UnknownFields
+	sizeCache     protoimpl.SizeCache
+}
+
+func (x *RefreshAccessTokenResponse) Reset() {
+	*x = RefreshAccessTokenResponse{}
+	mi := &file_auth_auth_proto_msgTypes[7]
+	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+	ms.StoreMessageInfo(mi)
+}
+
+func (x *RefreshAccessTokenResponse) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*RefreshAccessTokenResponse) ProtoMessage() {}
+
+func (x *RefreshAccessTokenResponse) ProtoReflect() protoreflect.Message {
+	mi := &file_auth_auth_proto_msgTypes[7]
+	if x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use RefreshAccessTokenResponse.ProtoReflect.Descriptor instead.
+func (*RefreshAccessTokenResponse) Descriptor() ([]byte, []int) {
+	return file_auth_auth_proto_rawDescGZIP(), []int{7}
+}
+
+func (x *RefreshAccessTokenResponse) GetAccessKey() string {
+	if x != nil {
+		return x.AccessKey
+	}
+	return ""
+}
+
 var File_auth_auth_proto protoreflect.FileDescriptor
 
 const file_auth_auth_proto_rawDesc = "" +
@@ -350,11 +446,19 @@ const file_auth_auth_proto_rawDesc = "" +
 	"\vrefresh_key\x18\x02 \x01(\tR\n" +
 	"refreshKey\"*\n" +
 	"\x0eLogoutResponse\x12\x18\n" +
-	"\asuccess\x18\x01 \x01(\bR\asuccess2\x8f\x01\n" +
-	"\x04Auth\x120\n" +
-	"\rCreateAccount\x12\x0e.LogoutRequest\x1a\r.LoginRequest\"\x00\x12(\n" +
+	"\asuccess\x18\x01 \x01(\bR\asuccess\"R\n" +
+	"\x19RefreshAccessTokenRequest\x12\x14\n" +
+	"\x05email\x18\x01 \x01(\tR\x05email\x12\x1f\n" +
+	"\vrefresh_key\x18\x02 \x01(\tR\n" +
+	"refreshKey\";\n" +
+	"\x1aRefreshAccessTokenResponse\x12\x1d\n" +
+	"\n" +
+	"access_key\x18\x01 \x01(\tR\taccessKey2\xdb\x01\n" +
+	"\x04Auth\x12+\n" +
+	"\x06Signup\x12\x0e.SignupRequest\x1a\x0f.SignupResponse\"\x00\x12(\n" +
 	"\x05Login\x12\r.LoginRequest\x1a\x0e.LoginResponse\"\x00\x12+\n" +
-	"\x06Logout\x12\x0e.LogoutRequest\x1a\x0f.LogoutResponse\"\x00B5Z3github.com/andicrypt/moodle-microservice-proto/authb\x06proto3"
+	"\x06Logout\x12\x0e.LogoutRequest\x1a\x0f.LogoutResponse\"\x00\x12O\n" +
+	"\x12RefreshAccessToken\x12\x1a.RefreshAccessTokenRequest\x1a\x1b.RefreshAccessTokenResponse\"\x00B5Z3github.com/andicrypt/moodle-microservice-proto/authb\x06proto3"
 
 var (
 	file_auth_auth_proto_rawDescOnce sync.Once
@@ -368,24 +472,28 @@ func file_auth_auth_proto_rawDescGZIP() []byte {
 	return file_auth_auth_proto_rawDescData
 }
 
-var file_auth_auth_proto_msgTypes = make([]protoimpl.MessageInfo, 6)
+var file_auth_auth_proto_msgTypes = make([]protoimpl.MessageInfo, 8)
 var file_auth_auth_proto_goTypes = []any{
-	(*SignupRequest)(nil),  // 0: SignupRequest
-	(*SignupResponse)(nil), // 1: SignupResponse
-	(*LoginRequest)(nil),   // 2: LoginRequest
-	(*LoginResponse)(nil),  // 3: LoginResponse
-	(*LogoutRequest)(nil),  // 4: LogoutRequest
-	(*LogoutResponse)(nil), // 5: LogoutResponse
+	(*SignupRequest)(nil),              // 0: SignupRequest
+	(*SignupResponse)(nil),             // 1: SignupResponse
+	(*LoginRequest)(nil),               // 2: LoginRequest
+	(*LoginResponse)(nil),              // 3: LoginResponse
+	(*LogoutRequest)(nil),              // 4: LogoutRequest
+	(*LogoutResponse)(nil),             // 5: LogoutResponse
+	(*RefreshAccessTokenRequest)(nil),  // 6: RefreshAccessTokenRequest
+	(*RefreshAccessTokenResponse)(nil), // 7: RefreshAccessTokenResponse
 }
 var file_auth_auth_proto_depIdxs = []int32{
-	4, // 0: Auth.CreateAccount:input_type -> LogoutRequest
+	0, // 0: Auth.Signup:input_type -> SignupRequest
 	2, // 1: Auth.Login:input_type -> LoginRequest
 	4, // 2: Auth.Logout:input_type -> LogoutRequest
-	2, // 3: Auth.CreateAccount:output_type -> LoginRequest
-	3, // 4: Auth.Login:output_type -> LoginResponse
-	5, // 5: Auth.Logout:output_type -> LogoutResponse
-	3, // [3:6] is the sub-list for method output_type
-	0, // [0:3] is the sub-list for method input_type
+	6, // 3: Auth.RefreshAccessToken:input_type -> RefreshAccessTokenRequest
+	1, // 4: Auth.Signup:output_type -> SignupResponse
+	3, // 5: Auth.Login:output_type -> LoginResponse
+	5, // 6: Auth.Logout:output_type -> LogoutResponse
+	7, // 7: Auth.RefreshAccessToken:output_type -> RefreshAccessTokenResponse
+	4, // [4:8] is the sub-list for method output_type
+	0, // [0:4] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
 	0, // [0:0] is the sub-list for extension extendee
 	0, // [0:0] is the sub-list for field type_name
@@ -402,7 +510,7 @@ func file_auth_auth_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: unsafe.Slice(unsafe.StringData(file_auth_auth_proto_rawDesc), len(file_auth_auth_proto_rawDesc)),
 			NumEnums:      0,
-			NumMessages:   6,
+			NumMessages:   8,
 			NumExtensions: 0,
 			NumServices:   1,
 		},
--- golang/auth/auth_grpc.pb.go
+++ golang/auth/auth_grpc.pb.go
@@ -19,18 +19,20 @@ import (
 const _ = grpc.SupportPackageIsVersion9
 
 const (
-	Auth_CreateAccount_FullMethodName = "/Auth/CreateAccount"
-	Auth_Login_FullMethodName         = "/Auth/Login"
-	Auth_Logout_FullMethodName        = "/Auth/Logout"
+	Auth_Signup_FullMethodName             = "/Auth/Signup"
+	Auth_Login_FullMethodName              = "/Auth/Login"
+	Auth_Logout_FullMethodName             = "/Auth/Logout"
+	Auth_RefreshAccessToken_FullMethodName = "/Auth/RefreshAccessToken"
 )
 
 // AuthClient is the client API for Auth service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type AuthClient interface {
-	CreateAccount(ctx context.Context, in *LogoutRequest, opts ...grpc.CallOption) (*LoginRequest, error)
+	Signup(ctx context.Context, in *SignupRequest, opts ...grpc.CallOption) (*SignupResponse, error)
 	Login(ctx context.Context, in *LoginRequest, opts ...grpc.CallOption) (*LoginResponse, error)
 	Logout(ctx context.Context, in *LogoutRequest, opts ...grpc.CallOption) (*LogoutResponse, error)
+	RefreshAccessToken(ctx context.Context, in *RefreshAccessTokenRequest, opts ...grpc.CallOption) (*RefreshAccessTokenResponse, error)
 }
 
 type authClient struct {
@@ -41,10 +43,10 @@ func NewAuthClient(cc grpc.ClientConnInterface) AuthClient {
 	return &authClient{cc}
 }
 
-func (c *authClient) CreateAccount(ctx context.Context, in *LogoutRequest, opts ...grpc.CallOption) (*LoginRequest, error) {
+func (c *authClient) Signup(ctx context.Context, in *SignupRequest, opts ...grpc.CallOption) (*SignupResponse, error) {
 	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
-	out := new(LoginRequest)
-	err := c.cc.Invoke(ctx, Auth_CreateAccount_FullMethodName, in, out, cOpts...)
+	out := new(SignupResponse)
+	err := c.cc.Invoke(ctx, Auth_Signup_FullMethodName, in, out, cOpts...)
 	if err != nil {
 		return nil, err
 	}
@@ -71,13 +73,24 @@ func (c *authClient) Logout(ctx context.Context, in *LogoutRequest, opts ...grpc
 	return out, nil
 }
 
+func (c *authClient) RefreshAccessToken(ctx context.Context, in *RefreshAccessTokenRequest, opts ...grpc.CallOption) (*RefreshAccessTokenResponse, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(RefreshAccessTokenResponse)
+	err := c.cc.Invoke(ctx, Auth_RefreshAccessToken_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // AuthServer is the server API for Auth service.
 // All implementations must embed UnimplementedAuthServer
 // for forward compatibility.
 type AuthServer interface {
-	CreateAccount(context.Context, *LogoutRequest) (*LoginRequest, error)
+	Signup(context.Context, *SignupRequest) (*SignupResponse, error)
 	Login(context.Context, *LoginRequest) (*LoginResponse, error)
 	Logout(context.Context, *LogoutRequest) (*LogoutResponse, error)
+	RefreshAccessToken(context.Context, *RefreshAccessTokenRequest) (*RefreshAccessTokenResponse, error)
 	mustEmbedUnimplementedAuthServer()
 }
 
@@ -88,14 +101,17 @@ type AuthServer interface {
 // pointer dereference when methods are called.
 type UnimplementedAuthServer struct{}
 
-func (UnimplementedAuthServer) CreateAccount(context.Context, *LogoutRequest) (*LoginRequest, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method CreateAccount not implemented")
+func (UnimplementedAuthServer) Signup(context.Context, *SignupRequest) (*SignupResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Signup not implemented")
 }
 func (UnimplementedAuthServer) Login(context.Context, *LoginRequest) (*LoginResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Login not implemented")
 }
 func (UnimplementedAuthServer) Logout(context.Context, *LogoutRequest) (*LogoutResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Logout not implemented")
+}
+func (UnimplementedAuthServer) RefreshAccessToken(context.Context, *RefreshAccessTokenRequest) (*RefreshAccessTokenResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method RefreshAccessToken not implemented")
 }
 func (UnimplementedAuthServer) mustEmbedUnimplementedAuthServer() {}
 func (UnimplementedAuthServer) testEmbeddedByValue()              {}
@@ -118,20 +134,20 @@ func RegisterAuthServer(s grpc.ServiceRegistrar, srv AuthServer) {
 	s.RegisterService(&Auth_ServiceDesc, srv)
 }
 
-func _Auth_CreateAccount_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(LogoutRequest)
+func _Auth_Signup_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(SignupRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(AuthServer).CreateAccount(ctx, in)
+		return srv.(AuthServer).Signup(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: Auth_CreateAccount_FullMethodName,
+		FullMethod: Auth_Signup_FullMethodName,
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(AuthServer).CreateAccount(ctx, req.(*LogoutRequest))
+		return srv.(AuthServer).Signup(ctx, req.(*SignupRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -172,6 +188,24 @@ func _Auth_Logout_Handler(srv interface{}, ctx context.Context, dec func(interfa
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Auth_RefreshAccessToken_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(RefreshAccessTokenRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(AuthServer).RefreshAccessToken(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: Auth_RefreshAccessToken_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(AuthServer).RefreshAccessToken(ctx, req.(*RefreshAccessTokenRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // Auth_ServiceDesc is the grpc.ServiceDesc for Auth service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -180,8 +214,8 @@ var Auth_ServiceDesc = grpc.ServiceDesc{
 	HandlerType: (*AuthServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
-			MethodName: "CreateAccount",
-			Handler:    _Auth_CreateAccount_Handler,
+			MethodName: "Signup",
+			Handler:    _Auth_Signup_Handler,
 		},
 		{
 			MethodName: "Login",
@@ -190,6 +224,10 @@ var Auth_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "Logout",
 			Handler:    _Auth_Logout_Handler,
+		},
+		{
+			MethodName: "RefreshAccessToken",
+			Handler:    _Auth_RefreshAccessToken_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
